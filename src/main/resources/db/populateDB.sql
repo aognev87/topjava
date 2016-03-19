0b8101744fa0ed138498ace38a596da9801bf5ef
Anton <aognev.87@yandex.ru>
2016-03-19 19:49:57 +0300

    HW03-meals-table-scripts

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -7,6 +8,14 @@ VALUES ('User', 'user@yandex.ru', 'password');
 
 INSERT INTO users (name, email, password)
 VALUES ('Admin', 'admin@gmail.com', 'admin');
+
+INSERT INTO meals (description, calories) VALUES
+  ('Завтрак', 500),
+  ('Обед', 1000),
+  ('Ужин', 500),
+  ('Завтрак', 1000),
+  ('Обед', 500),
+  ('Ужин', 510);
 
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
